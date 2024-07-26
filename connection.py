558f6ba660d684b2429ae87b6df882f3250c8c09
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-07-26 11:08:53 +0300

    deplacement de dockerfile

--- connection.py
+++
@@ -1,7 +0,0 @@
-config = {
-        'user': 'admin',
-        'password': 'Un234567-8',
-        'host': 'mybooks.c36k8emmg1ow.us-east-1.rds.amazonaws.com',
-        'port': '3306',
-        'database': 'mybooks'
-    }
