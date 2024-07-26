5201886ca18a2a7a9341a0362d7ac397269f5b93
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-07-26 10:56:27 +0300

    add docker.yml

---
+++ app/connection.py
@@ -0,0 +1,7 @@
+config = {
+        'user': 'admin',
+        'password': 'Un234567-8',
+        'host': 'mybooks.c36k8emmg1ow.us-east-1.rds.amazonaws.com',
+        'port': 3306,
+        'database': 'mybooks'
+    }
